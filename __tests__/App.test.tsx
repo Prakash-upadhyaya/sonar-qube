a31c57e52c33d0342484c4b78f2d91b13969d047
Prakash-upadhyaya <connect.upadhya@gmail.com>
2025-03-23 12:15:47 +0530

    initial project setup

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -1,13 +1,6 @@
-/**
- * @format
- */
-
 import React from 'react';
-import ReactTestRenderer from 'react-test-renderer';
 import App from '../App';
-
+import { render } from '@testing-library/react-native'
 test('renders correctly', async () => {
-  await ReactTestRenderer.act(() => {
-    ReactTestRenderer.create(<App />);
-  });
+  render(<App />)
 });
